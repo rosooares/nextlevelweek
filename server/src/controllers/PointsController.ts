e397e0f73fe92b85061975ed13c0726ed2d99fc3
Roberta Soares <kestraa_2@Silvios-MacBook-Pro.local>
2020-06-03 18:21:11 -0300

    query param correction

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -3,14 +3,13 @@ import knex from '../database/connection';
 
 class PointsController {
   async index(request: Request, response: Response) {
-    // cidade, uf, items (query params)
     const { city, uf, items } = request.query;
     const parsedItems = String(items)
       .split(',')
       .map(item => Number(item.trim()));
 
     const points = await knex('points')
-      .join('point_items', 'points.id', '=', 'points_items.point_id')
+      .join('point_items', 'points.id', '=', 'point_items.point_id')
       .whereIn('point_items.item_id', parsedItems)
       .where('city', String(city))
       .where('uf', String(uf))
