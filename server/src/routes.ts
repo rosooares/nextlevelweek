6e497fc464e9701faa0c8c2a5cb8c9d0d562fb0b
Roberta Soares <kestraa_2@Silvios-MacBook-Pro.local>
2020-06-06 17:39:35 -0300

    validation with celebrate - server backend

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,6 +3,7 @@ import PoitsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
 import multer from 'multer';
 import multerConfig from './config/multer';
+import { celebrate, Joi } from 'celebrate';
 
 const routes = express.Router();
 const upload = multer(multerConfig);
@@ -15,6 +16,24 @@ routes.get('/items', itemsController.index);
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
 
-routes.post('/points', upload.single('image') ,pointsController.create);
+routes.post(
+  '/points',
+  upload.single('image'),
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.number().required(),
+      uf: Joi.string().required().max(2),
+      city: Joi.string().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      items: Joi.string().required(),
+    })
+  }, {
+    abortEarly: false,
+  }),
+  pointsController.create
+);
 
 export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import cors from 'cors';
 import routes from './routes';
+import { errors } from 'celebrate';
 
 const app = express();
 
@@ -10,5 +11,7 @@ app.use(express.json());
 app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 app.listen(3333);
